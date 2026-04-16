8ed7e4e6fd5e0a7c1bd9cb67c6a583a98ed0e9b7
baorng <baorng@gmail.com>
2026-04-16 17:53:02 +0800

    Update questions

--- src/lib/scoring.test.js
+++ src/lib/scoring.test.js
@@ -28,11 +28,11 @@ describe('scoring logic', () => {
       responses[id - 1] = 5
     })
 
-    ;[10, 11, 12, 13, 14, 15, 16, 17, 18].forEach((id) => {
+    ;[28, 29, 30, 31, 32, 33, 34, 35, 36].forEach((id) => {
       responses[id - 1] = 5
     })
 
-    ;[28, 29, 30, 31, 32, 33, 34, 35, 36].forEach((id) => {
+    ;[19, 20, 21, 22, 23, 24, 25, 26, 27].forEach((id) => {
       responses[id - 1] = 2
     })
 
@@ -40,8 +40,8 @@ describe('scoring logic', () => {
       responses[id - 1] = 2
     })
 
-    ;[19, 20, 21, 22, 23, 24, 25, 26, 27].forEach((id) => {
-      responses[id - 1] = 5
+    ;[10, 11, 12, 13, 14, 15, 16, 17, 18].forEach((id) => {
+      responses[id - 1] = 2
     })
 
     const assessment = computeAssessment(responses)
